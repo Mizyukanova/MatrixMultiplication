c72946e91b80638701de5f06dccadcc0bf54c4ff
Mizyukanova <mizyukanova.ane4ka@mail.ru>
2021-02-21 16:40:25 +0700

    Добавлен тест 3: перемножение матрицы на вектор.

---
+++ test3.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <time.h>
+#include <malloc.h>
+#include <math.h>
+#include "MatrixMultiplication.h"
+
+int ComparisonResults(const float * C, const float * C1, const long int size)
+{
+    double epsilon=1e-07;
+
+    for (int i = 0; i < size; ++i)
+    {
+        if ( fabs(C[i]/C1[i] - 1) > epsilon )
+        {
+            return 1;
+        }
+    }
+    
+    return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+    const int m=3, n=1, k=2, threads=2;
+    float A[6]={1, 2, 3, 4, 5, 6};
+    float B[2]={7, 8};
+    float C[3]={23, 53, 83};
+    float *C1, *C2;
+    
+    /* Размерности матриц: A(MxK), B(KxN), C(MxN) и количество потоков*/
+    printf("M=%d\n", m);
+    printf("N=%d\n", n);
+    printf("K=%d\n", k);
+    printf("Threads=%d\n", threads);
+    
+    /* Выделение памяти под матрицы*/
+    C1=(float*)malloc(m * n * sizeof(float));
+    C2=(float*)malloc(m * n * sizeof(float));
+    
+    MatrixMultiplicationSimple(threads,m,n,k,A,B,C1);
+    
+    MatrixMultiplicationOptimized(threads,m,n,k,A,B,C2);
+    
+    /* Вывод матриц*/
+    int i,j;
+    printf("A\n");
+    for (i=0; i<m; i++)
+    {
+        for (j=0; j<k; j++)
+        {
+            printf("%f\t", A[i*k+j]);
+        }
+        printf("\n");  
+    }
+
+    printf("B\n");
+    for (i= 0; i<k; i++)
+    {
+        for (j= 0; j<n; j++)
+        {
+            printf("%f\t", B[i*n+j]);
+        }
+        printf("\n");  
+    }
+    printf("C(Expected)\n");
+    for (i= 0; i<m; i++)
+    {
+        for (j= 0; j<n; j++)
+        {
+            printf("%f\t", C[i*n+j]);
+        }
+        printf("\n");  
+    }
+    
+    /* Сравнение результатов перемножения*/
+    printf("C(Simple)\n");
+    for (i= 0; i<m; i++)
+    {
+        for (j= 0; j<n; j++)
+        {
+            printf("%f\t", C1[i*n+j]);
+        }
+        printf("\n");  
+    }
+    if ( ComparisonResults(C, C1, (long int)m*n) == 0)
+    {
+        printf("Test 1 Simple passed.\n");
+    }
+    else
+    {
+        printf("Test 1 Simple failed.\n");
+    }
+    
+    printf("C(Optimized)\n");
+    for (i= 0; i<m; i++)
+    {
+        for (j= 0; j<n; j++)
+        {
+            printf("%f\t", C2[i*n+j]);
+        }
+        printf("\n");  
+    }
+    if ( ComparisonResults(C, C2, (long int)m*n) == 0)
+    {
+        printf("Test 1 Optimized passed.\n" );
+    }
+    else
+    {
+        printf("Test 1 Optimized failed.\n");
+    }
+
+    /* Освобождение памяти*/
+    free(C1);
+    free(C2);
+
+    return 0;
+}
+
